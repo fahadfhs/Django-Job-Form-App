2620e34158c43d64855c58ffa22c0cd118277909
fahadfhs <90725219+fahadfhs@users.noreply.github.com>
2023-09-30 23:28:42 -0400

    created more functionality in admin field by adding list_display, search field, filtering adn more. Adds more dynamic assistance to the form

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from . models import Form
+from .models import Form
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")  # lets you display the details in the admin page
+    search_fields = ("first_name", "last_name", "email")  # shows the search field
+    list_filter = ("date", "occupation")  # helps you filter data
+    ordering = ("-first_name",)  # shows the name in ordered or descending in -
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
